78859a240c6ba403d2735b97d453c8530e292f8f
Haseeb Majid <hmajid2301@gmail.com>
2018-11-24 12:20:45 +0000

    :sparkles: Article 8
    Written article 8, published here, https://medium.com/me/stories/public.

---
+++ 8. Docker with SQLAlchemy/source_code/src/example/app.py
@@ -0,0 +1,49 @@
+import json
+
+from flask import request
+
+from . import create_app, database
+from .models import Cats, db
+
+app = create_app()
+
+
+@app.route('/', methods=['GET'])
+def fetch():
+    cats = database.get_all(Cats)
+    all_cats = []
+    for cat in cats:
+        new_cat = {
+            "id": cat.id,
+            "name": cat.name,
+            "price": cat.price,
+            "breed": cat.breed
+        }
+
+        all_cats.append(new_cat)
+    return json.dumps(all_cats), 200
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    data = request.get_json()
+    name = data['name']
+    price = data['price']
+    breed = data['breed']
+
+    database.add_instance(Cats, name=name, price=price, breed=breed)
+    return json.dumps("Added"), 200
+
+
+@app.route('/remove/<cat_id>', methods=['DELETE'])
+def remove(cat_id):
+    database.delete_instance(Cats, id=cat_id)
+    return json.dumps("Deleted"), 200
+
+
+@app.route('/edit/<cat_id>', methods=['PATCH'])
+def edit(cat_id):
+    data = request.get_json()
+    new_price = data['price']
+    database.update_instance(Cats, id=cat_id, price=new_price)
+    return json.dumps("Edited"), 200
