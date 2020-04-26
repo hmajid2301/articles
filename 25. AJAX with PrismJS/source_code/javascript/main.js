478a469dbcf6935f336463e66854d768bf1e4409
Haseeb Majid <hello@haseebmajid.dev>
2020-04-26 14:07:27 +0100

    2o4k

--- 25. AJAX with PrismJS/source_code/javascript/main.js
+++ 25. AJAX with PrismJS/source_code/javascript/main.js
@@ -1,8 +1,7 @@
-function reRunPrismJS() {
-  const yamlEditable = document.getElementById("editable");
-  const dockerCompose = yamlEditable.innerText;
-  yamlEditable.innerHTML = "";
-  yamlEditable.innerHTML += '<code id="yaml" class="language-yaml"></code>';
+function onPaste() {
+  const editable = document.getElementById("editable");
+  const dockerCompose = editable.innerText;
+  editable.innerHTML = '<code id="yaml" class="language-yaml"></code>';
   const yaml = document.getElementById("yaml");
   yaml.innerHTML = Prism.highlight(dockerCompose, Prism.languages.yaml, "yaml");
 }
